9e1c927ac906ed46e55b795546dbae875c512f51
Denis <denisivov437@gmail.com>
2025-03-18 06:05:18 +0200

    made some tasks.

---
+++ 02-ReverseArrayOFIntegers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_ReverseArrayOFIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var number = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                number[i] = int.Parse(Console.ReadLine());
+            }
+            for(int i = n-1 ; i >= 0; i--)
+            {
+                Console.WriteLine(number[i]);
+            }
+        }
+    }
+}
