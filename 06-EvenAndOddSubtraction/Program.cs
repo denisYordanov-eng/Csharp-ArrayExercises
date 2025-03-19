c45dc02544c99ac51b2bb8186480f1f20a1d804e
Denis <denisivov437@gmail.com>
2025-03-19 16:17:26 +0200

    made a task.

---
+++ 06-EvenAndOddSubtraction/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_EvenAndOddSubtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int sumEven = 0;
+            int sumOdd = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNumber = numbers[i];
+                if(currentNumber % 2 == 0)
+                {
+                    sumEven += currentNumber;
+                }
+                else if(currentNumber % 2 != 0)
+                {
+                    sumOdd += currentNumber;
+                }
+            }
+            int diff = sumEven - sumOdd;
+            Console.WriteLine(diff);
+        }
+    }
+}
