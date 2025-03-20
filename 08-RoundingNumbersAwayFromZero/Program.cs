fb410bb55a6b6301e2436c27c7fe81b7740b6df5
Denis <denisivov437@gmail.com>
2025-03-20 15:25:40 +0200

    just made a task about rounding numbers using new way of rounding.

---
+++ 08-RoundingNumbersAwayFromZero/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_RoundingNumbersAwayFromZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            double[] values = input.Select(double.Parse).ToArray();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                double roundNumber = (values[i]);
+                Console.WriteLine("{0} => {1}", values[i], Math.Round(roundNumber, MidpointRounding.AwayFromZero));
+            }
+        }
+    }
+}
