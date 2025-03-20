a7ace90fe8f1003ce0ed06f6d05a581876dbcdd1
Denis <denisivov437@gmail.com>
2025-03-20 09:39:46 +0200

    made the fourth task.

---
+++ 04-ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ReverseArrayOfStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            string[] words = Console.ReadLine().Split(' ').ToArray();
+            Console.WriteLine(String.Join(" ", words.Reverse()));
+           
+        }
+    }    
+}
