3dcdb1cd82d7519e7f4c51e0097e112da6ecf0ff
Denis <denisivov437@gmail.com>
2025-03-20 13:24:53 +0200

    just made another solution for four task.

---
+++ 04-ReverseArrayOfStrings-AnotherWay/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ReverseArrayOfStrings_AnotherWay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] words = input.Split(' ');
+            string[] reversedWords = new string[words.Length];
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                reversedWords[i] = words[words.Length - 1 - i];
+            }
+           
+                Console.WriteLine(String.Join(" ", reversedWords));
+            
+        }
+    }
+}
