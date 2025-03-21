060d88b20ca323e0d4ce4341280850c4b18a6c5b
Denis <denisivov437@gmail.com>
2025-03-21 07:05:25 +0200

    just made a task.

---
+++ 11-LastKNumbersSumsSequence/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_LastKNumbersSumsSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long number = long.Parse(Console.ReadLine());
+            long step = long.Parse(Console.ReadLine());
+
+            long[] sequence = new long[number];
+            sequence[0] = 1;
+            for (long i = 1; i < number; i++)
+            {
+                long sum = 0;
+               long toSteps = 1;
+                long last = i - 1;
+
+              while(last >= 0 && toSteps <= step)
+                {
+                    sum += sequence[last--];
+                    toSteps++;
+                }
+                sequence[i] = sum;
+            }
+
+            foreach (var value in sequence)
+            {
+                Console.Write(value + " ");
+            }
+        }
+    }
+}
